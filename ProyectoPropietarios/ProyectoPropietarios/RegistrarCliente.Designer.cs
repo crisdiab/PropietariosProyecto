ebcce14f21c404a54a031368e9f204c4db6ddf22
RaulAkd <molinaraul5219@gmail.com>
2017-01-26 06:35:32 -0500

    ventanaRegistrarCliente

---
+++ ProyectoPropietarios/ProyectoPropietarios/RegistrarCliente.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace ProyectoPropietarios
+{
+    partial class RegistrarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbIdCE = new System.Windows.Forms.Label();
+            this.lbIdRepresentante = new System.Windows.Forms.Label();
+            this.lbIdServicio = new System.Windows.Forms.Label();
+            this.lbIdPaciente = new System.Windows.Forms.Label();
+            this.txtIdCE = new System.Windows.Forms.TextBox();
+            this.txtIdRepresentante = new System.Windows.Forms.TextBox();
+            this.txtIdServicio = new System.Windows.Forms.TextBox();
+            this.lblDatosPersonales = new System.Windows.Forms.Label();
+            this.txtIdPaciente = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(513, 169);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 13);
+            this.label10.TabIndex = 64;
+            this.label10.Text = "Estado";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(442, 197);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(179, 20);
+            this.textBox7.TabIndex = 61;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(442, 234);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(179, 60);
+            this.textBox5.TabIndex = 59;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(442, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 20);
+            this.textBox4.TabIndex = 58;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(442, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 20);
+            this.textBox3.TabIndex = 57;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(442, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 20);
+            this.textBox2.TabIndex = 56;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(442, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 55;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Location = new System.Drawing.Point(575, 168);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 54;
+            this.checkBox1.Text = "Activo";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(354, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 51;
+            this.label7.Text = "Teléfono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(354, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Dirección";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(351, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 48;
+            this.label4.Text = "Edad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(351, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Cédula";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(351, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Apellido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(351, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Nombre";
+            // 
+            // lbIdCE
+            // 
+            this.lbIdCE.AutoSize = true;
+            this.lbIdCE.Location = new System.Drawing.Point(379, 8);
+            this.lbIdCE.Name = "lbIdCE";
+            this.lbIdCE.Size = new System.Drawing.Size(29, 13);
+            this.lbIdCE.TabIndex = 42;
+            this.lbIdCE.Text = "idCE";
+            // 
+            // lbIdRepresentante
+            // 
+            this.lbIdRepresentante.AutoSize = true;
+            this.lbIdRepresentante.Location = new System.Drawing.Point(246, 8);
+            this.lbIdRepresentante.Name = "lbIdRepresentante";
+            this.lbIdRepresentante.Size = new System.Drawing.Size(85, 13);
+            this.lbIdRepresentante.TabIndex = 41;
+            this.lbIdRepresentante.Text = "idRepresentante";
+            // 
+            // lbIdServicio
+            // 
+            this.lbIdServicio.AutoSize = true;
+            this.lbIdServicio.Location = new System.Drawing.Point(145, 8);
+            this.lbIdServicio.Name = "lbIdServicio";
+            this.lbIdServicio.Size = new System.Drawing.Size(53, 13);
+            this.lbIdServicio.TabIndex = 40;
+            this.lbIdServicio.Text = "idServicio";
+            // 
+            // lbIdPaciente
+            // 
+            this.lbIdPaciente.AutoSize = true;
+            this.lbIdPaciente.Location = new System.Drawing.Point(42, 8);
+            this.lbIdPaciente.Name = "lbIdPaciente";
+            this.lbIdPaciente.Size = new System.Drawing.Size(57, 13);
+            this.lbIdPaciente.TabIndex = 39;
+            this.lbIdPaciente.Text = "idPaciente";
+            // 
+            // txtIdCE
+            // 
+            this.txtIdCE.Location = new System.Drawing.Point(414, 5);
+            this.txtIdCE.Name = "txtIdCE";
+            this.txtIdCE.Size = new System.Drawing.Size(36, 20);
+            this.txtIdCE.TabIndex = 38;
+            // 
+            // txtIdRepresentante
+            // 
+            this.txtIdRepresentante.Location = new System.Drawing.Point(337, 5);
+            this.txtIdRepresentante.Name = "txtIdRepresentante";
+            this.txtIdRepresentante.Size = new System.Drawing.Size(36, 20);
+            this.txtIdRepresentante.TabIndex = 37;
+            // 
+            // txtIdServicio
+            // 
+            this.txtIdServicio.Location = new System.Drawing.Point(204, 5);
+            this.txtIdServicio.Name = "txtIdServicio";
+            this.txtIdServicio.Size = new System.Drawing.Size(36, 20);
+            this.txtIdServicio.TabIndex = 36;
+            // 
+            // lblDatosPersonales
+            // 
+            this.lblDatosPersonales.AutoSize = true;
+            this.lblDatosPersonales.BackColor = System.Drawing.Color.Transparent;
+            this.lblDatosPersonales.Location = new System.Drawing.Point(439, 39);
+            this.lblDatosPersonales.MaximumSize = new System.Drawing.Size(400, 400);
+            this.lblDatosPersonales.Name = "lblDatosPersonales";
+            this.lblDatosPersonales.Size = new System.Drawing.Size(119, 13);
+            this.lblDatosPersonales.TabIndex = 35;
+            this.lblDatosPersonales.Text = "DATOS PERSONALES";
+            // 
+            // txtIdPaciente
+            // 
+            this.txtIdPaciente.Location = new System.Drawing.Point(105, 5);
+            this.txtIdPaciente.Name = "txtIdPaciente";
+            this.txtIdPaciente.Size = new System.Drawing.Size(34, 20);
+            this.txtIdPaciente.TabIndex = 34;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::ProyectoPropietarios.Properties.Resources.retroceso;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(229, 267);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(58, 53);
+            this.pictureBox4.TabIndex = 66;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::ProyectoPropietarios.Properties.Resources.borrar;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(148, 267);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(58, 53);
+            this.pictureBox3.TabIndex = 65;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::ProyectoPropietarios.Properties.Resources.aceptar;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(81, 267);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 53);
+            this.pictureBox2.TabIndex = 44;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::ProyectoPropietarios.Properties.Resources.iconoDatosPersonales;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(81, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 186);
+            this.pictureBox1.TabIndex = 43;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RegistrarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 368);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbIdCE);
+            this.Controls.Add(this.lbIdRepresentante);
+            this.Controls.Add(this.lbIdServicio);
+            this.Controls.Add(this.lbIdPaciente);
+            this.Controls.Add(this.txtIdCE);
+            this.Controls.Add(this.txtIdRepresentante);
+            this.Controls.Add(this.txtIdServicio);
+            this.Controls.Add(this.lblDatosPersonales);
+            this.Controls.Add(this.txtIdPaciente);
+            this.Name = "RegistrarCliente";
+            this.Text = "RegistrarCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbIdCE;
+        private System.Windows.Forms.Label lbIdRepresentante;
+        private System.Windows.Forms.Label lbIdServicio;
+        private System.Windows.Forms.Label lbIdPaciente;
+        private System.Windows.Forms.TextBox txtIdCE;
+        private System.Windows.Forms.TextBox txtIdRepresentante;
+        private System.Windows.Forms.TextBox txtIdServicio;
+        private System.Windows.Forms.Label lblDatosPersonales;
+        private System.Windows.Forms.TextBox txtIdPaciente;
+    }
+}
