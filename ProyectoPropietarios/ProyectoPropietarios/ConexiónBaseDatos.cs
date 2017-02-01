17600eb2b34be2c49208591a6ea92c9133f0dedf
crisdiab <cristian89lara@gmail.com>
2017-02-01 14:47:27 -0500

    Agregado clases de conexion a bdd y conculta a bdd

---
+++ ProyectoPropietarios/ProyectoPropietarios/ConexiónBaseDatos.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace ProyectoPropietarios
+{
+   
+    
+        class ConexiónBaseDatos
+        {
+            public void ExecuteNonQuery(string strSQL)
+            {
+                SqlConnection cnn = null;
+
+                try
+                {
+                    cnn = new SqlConnection(ObtenerCadenaConexion());
+                    cnn.Open();
+                    SqlCommand cmd = cnn.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = strSQL;
+
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (cnn.State == ConnectionState.Open)
+                    {
+                        cnn.Close();
+                    }
+                }
+            }
+
+            public DataSet ExecuteDataSet(string strSQL)
+            {
+                DataSet ds = new DataSet();
+                SqlConnection cnn = null;
+
+
+                try
+                {
+                    cnn = new SqlConnection(ObtenerCadenaConexion());
+
+                    cnn.Open();
+                    SqlCommand cmd = cnn.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = strSQL;
+
+
+                    cmd.CommandTimeout = 0; // infinito
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    da.Fill(ds, "Tabla1");
+                    return ds;
+                }
+
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (cnn.State == ConnectionState.Open)
+                    {
+                        cnn.Close();
+                    }
+                }
+
+
+            }
+
+            private string ObtenerCadenaConexion()
+            {
+                try
+                {
+
+
+                    return "DATA SOURCE = localhost; Initial Catalog = BaseDesarrolloV1; Integrated Security = True";
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    
+
+}
---
+++ ProyectoPropietarios/ProyectoPropietarios/ConsultasBaseDatos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace ProyectoPropietarios
+{
+    class ConsultasBaseDatos
+    {
+        ConexiónBaseDatos conex = new ConexiónBaseDatos();
+
+        //solo para consultas 
+        public DataTable consultar(String consulta)
+        {
+            DataSet DS = conex.ExecuteDataSet(consulta);
+            DataTable DT = DS.Tables[0];
+            return DT;
+
+        }
+
+
+        //insert delet update
+        public void counsultaTodoTipo(String ingreso)
+        {
+            DataSet DS = conex.ExecuteDataSet(ingreso);
+        }
+    }
+
+}
